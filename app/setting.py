c15653590e4343d21cbe9becf81f679fa17251a7
greybeetle <hanxuan_g@163.com>
2022-08-09 19:36:46 +0800

    change a litter

--- app/setting.py
+++ app/setting.py
@@ -1,5 +1,5 @@
 class Default(object):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:**@124.221.249.48/website?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:*@124.221.249.48/website?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = 'asl;nadflkgaasdfasyfgher'   # 这里需要一个非常复杂的密码字符串
 
