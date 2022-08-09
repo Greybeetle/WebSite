ef65032b68084aa8b99b98307bf368ee1a9ea604
greybeetle <hanxuan_g@163.com>
2022-08-09 22:40:15 +0800

    add config

---
+++ app/config.py
@@ -0,0 +1,7 @@
+SQL_CONFIG = {
+    'SQL_IP': '*.*.*.*',
+    'SQL_USER': '****',
+    'SQL_PASSWORD': '****',
+    'DATABASE': '****'
+}
+SECRET_KEY = 'asl;nadflkgaasdfasyfgher'   # 这里需要一个非常复杂的密码字符串
