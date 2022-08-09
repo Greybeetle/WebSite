0f6a8fe4f15ccbf9a2c210ea2eef0def3af16735
greybeetle <hanxuan_g@163.com>
2022-08-09 22:34:50 +0800

    change code

---
+++ app/config.py
@@ -0,0 +1,7 @@
+SQL_CONFIG = {
+    'SQL_IP': '124.221.249.48',
+    'SQL_USER': 'root',
+    'SQL_PASSWORD': 'ghx19940727,.',
+    'DATABASE': 'website'
+}
+SECRET_KEY = 'asl;nadflkgaasdfasyfgher'   # 这里需要一个非常复杂的密码字符串
--- app/setting.py
+++ app/setting.py
@@ -1,5 +1,8 @@
+from .config import SQL_CONFIG, SECRET_KEY
+
+
 class Default(object):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:**@124.221.249.48/website?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://%s:%s@%s/%s?charset=utf8' % (SQL_CONFIG['SQL_USER'], SQL_CONFIG['SQL_PASSWORD'], SQL_CONFIG['SQL_IP'], SQL_CONFIG['DATABASE'])
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = 'asl;nadflkgaasdfasyfgher'   # 这里需要一个非常复杂的密码字符串
 
